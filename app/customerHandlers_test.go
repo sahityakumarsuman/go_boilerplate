7536d2b034942b7593100353ab27bbad63793437
Sahitya Kumar Suman <sahityakumarsuman@Sahityas-MBP.domain.name>
2021-07-06 17:03:01 +0530

    initial commit

---
+++ app/customerHandlers_test.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ashishjuyal/banking-lib/errs"
+	"github.com/ashishjuyal/banking/dto"
+	"github.com/ashishjuyal/banking/mocks/service"
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+)
+
+var router *mux.Router
+var ch CustomerHandlers
+var mockService *service.MockCustomerService
+
+func setup(t *testing.T) func() {
+	ctrl := gomock.NewController(t)
+	mockService = service.NewMockCustomerService(ctrl)
+	ch = CustomerHandlers{mockService}
+	router = mux.NewRouter()
+	router.HandleFunc("/customers", ch.getAllCustomers)
+	return func() {
+		router = nil
+		defer ctrl.Finish()
+	}
+}
+
+func Test_should_return_customers_with_status_code_200(t *testing.T) {
+	// Arrange
+	teardown := setup(t)
+	defer teardown()
+
+	dummyCustomers := []dto.CustomerResponse{
+		{"1001", "Sahitya", "New Delhi", "110011", "2021-03-06", "1"},
+		{"1002", "Kumar", "New Delhi", "110011", "2021-03-06", "1"},
+	}
+	mockService.EXPECT().GetAllCustomer("").Return(dummyCustomers, nil)
+	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
+
+	// Act
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	if recorder.Code != http.StatusOK {
+		t.Error("Failed while testing the status code")
+	}
+}
+
+func Test_should_return_status_code_500_with_error_message(t *testing.T) {
+	// Arrange
+	teardown := setup(t)
+	defer teardown()
+	mockService.EXPECT().GetAllCustomer("").Return(nil, errs.NewUnexpectedError("some database error"))
+	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
+
+	// Act
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	if recorder.Code != http.StatusInternalServerError {
+		t.Error("Failed while testing the status code")
+	}
+}
